9deb9256ff407d0221fc468c569d72183590e848
Meetkorat38 <meetkorat903@gmail.com>
2024-09-23 09:59:37 +0530

    changes header section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,19 +1,42 @@
+"use client"
+
+import { useState } from "react";
+
 export const Header = () => {
+  const [activeButton, setActiveButton] = useState<string>('home');
+
+  const handleClick = (button:string) => {
+    setActiveButton(button);
+  };
+
   return (
-    <div className="flex justify-center items-center fixed w-full  top-3 z-30">
+    <div className="flex justify-center items-center fixed w-full top-3 z-30">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item ">
+        <a
+          href="#"
+          className={`nav-item ${activeButton === 'home' ? 'bg-white text-black' : ''}`}
+          onClick={() => handleClick('home')}
+        >
           Home
         </a>
-        <a href="#projects" className="nav-item">
+        <a
+          href="#projects"
+          className={`nav-item ${activeButton === 'projects' ? 'bg-white text-black' : ''}`}
+          onClick={() => handleClick('projects')}
+        >
           Projects
         </a>
-        <a href="#about" className="nav-item">
+        <a
+          href="#about"
+          className={`nav-item ${activeButton === 'about' ? 'bg-white text-black' : ''}`}
+          onClick={() => handleClick('about')}
+        >
           About
         </a>
         <a
           href="#contact"
-          className="nav-item bg-white text-gray-950 hover:bg-white/70 hover:text-gray-900"
+          className={`nav-item ${activeButton === 'contact' ? 'bg-white text-black' : ''}`}
+          onClick={() => handleClick('contact')}
         >
           Contact
         </a>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -67,7 +67,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project, index) => (
             <div
               key={project.title}
-              className="  "
+              className=""
               // style={{ top: `calc(56px + ${index * 20}px)` }}
             >
               <div className="bg-gray-800 px-8 pt-8 md:pt-12 md:px-10 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:pointer-events-none after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20 lg:pt-16 lg:px-20">
