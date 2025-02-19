8feb2246e7efcb350113b483b3889d301a97c390
Meetkorat38 <meetkorat903@gmail.com>
2025-02-19 09:34:44 +0530

    Add Gmail icon and contact link to Contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import { CiLinkedin } from "react-icons/ci";
+import { IoMail } from "react-icons/io5";
+
 
 export const ContactSection = () => {
   return (
@@ -15,10 +18,14 @@ export const ContactSection = () => {
                 opportunities to be part of your visions.
               </p>
             </div>
-            <div className="z-20">
-              <a href="https://www.linkedin.com/in/meet-korat-335632245/" target="_blank" className="text-white cursor-pointer  bg-gray-900 inline-flex items-center px-6 h-12 md:h-16 rounded-xl mt-5 md:mt-0 gap-2 w-max">
+            <div className="z-20 flex flex-col lg:gap-4">
+              <a href="https://www.linkedin.com/in/meet-korat-335632245/" target="_blank" className="text-white cursor-pointer  bg-gray-900 inline-flex items-center px-6 h-12 md:h-14 rounded-xl mt-5 md:mt-0 gap-2 w-max">
                 <span className="font-semibold lg:text-lg">Get In Touch</span>
-                <CiLinkedin className="size-6 lg:size-8" />
+                <CiLinkedin className="size-6 lg:size-8 text-blue-500" />
+              </a>
+              <a href="mailto:meetkorat903@gmail.com" target="_blank" className="text-white cursor-pointer  bg-gray-900 inline-flex items-center px-6 h-12 md:h-14 rounded-xl mt-5 md:mt-0 gap-3 w-max">
+                <span className="font-semibold lg:text-lg">Contact Me</span>
+                <Image src={"/gmail.svg"} alt="Gmail" height={5} width={5} className="size-6 lg:size-7 " />
               </a>
             </div>
           </div>
@@ -27,3 +34,4 @@ export const ContactSection = () => {
     </section>
   );
 };
+``
